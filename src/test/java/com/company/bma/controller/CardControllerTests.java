bf5de6a3e6325f1f0fad7eae9c1ce23082a67af1
rakesh <rakesh8693@gmail.com>
2020-09-08 20:45:04 +0530

    fix: Added test resource files

--- src/test/java/com/company/bma/controller/CardControllerTests.java
+++ src/test/java/com/company/bma/controller/CardControllerTests.java
@@ -3,17 +3,14 @@ package com.company.bma.controller;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -26,6 +23,7 @@ import com.company.bma.model.Card;
 @RunWith(SpringRunner.class)
 @WebMvcTest(CardController.class)
 @AutoConfigureMockMvc(addFilters = false)
+@TestPropertySource("classpath:application.properties")
 public class CardControllerTests {
 	@Autowired
 	private MockMvc mockMvc;
