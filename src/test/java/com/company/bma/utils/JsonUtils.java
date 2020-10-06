06dfad42690666fb77399d50c69051f7a397f90b
amit tomar <amit.tomar1101@gmail.com>
2020-10-06 15:59:31 +0530

    Update JsonUtils.java
    test commit to triger build.

--- src/test/java/com/company/bma/utils/JsonUtils.java
+++ src/test/java/com/company/bma/utils/JsonUtils.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class JsonUtils {
 	public static String asJsonString(final Object obj) {
 		try {
+			System.out.println("test build");
 			return new ObjectMapper().writeValueAsString(obj);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
